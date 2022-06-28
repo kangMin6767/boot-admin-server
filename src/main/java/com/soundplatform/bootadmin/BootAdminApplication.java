591aa3029f547a15d74231eafc4d2f790e0c9a6b
kang <gangmingyu@gangmingyuui-Macmini.local>
2022-06-28 10:46:11 +0900

    admin server 구축

---
+++ src/main/java/com/soundplatform/bootadmin/BootAdminApplication.java
@@ -0,0 +1,16 @@
+package com.soundplatform.bootadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@EnableAdminServer
+@SpringBootApplication
+public class BootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAdminApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/soundplatform/bootadmin/WebSecurityConfig.java
@@ -0,0 +1,85 @@
+package com.soundplatform.bootadmin;
+
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.UUID;
+
+/**
+ * https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+ */
+
+@Configuration
+public class WebSecurityConfig {
+
+    private final AdminServerProperties adminServer;
+
+    public WebSecurityConfig(AdminServerProperties adminServer) {
+        this.adminServer = adminServer;
+    }
+
+    @Bean
+    public SavedRequestAwareAuthenticationSuccessHandler successHandler() {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler =
+                new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(this.adminServer.getContextPath() + "/");
+        return successHandler;
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+
+        http
+                .authorizeRequests()
+                .antMatchers(this.adminServer.getContextPath() + "/assets/**").permitAll()
+                .antMatchers(this.adminServer.getContextPath() + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage(this.adminServer.getContextPath() + "/login")
+                .successHandler(successHandler())
+                .and()
+                .logout()
+                .logoutUrl(this.adminServer.getContextPath() + "/logout")
+                .and()
+                .httpBasic()
+                .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringRequestMatchers(
+                        new AntPathRequestMatcher(this.adminServer.getContextPath() +
+                                "/instances", HttpMethod.POST.toString()),
+                        new AntPathRequestMatcher(this.adminServer.getContextPath() +
+                                "/instances/*", HttpMethod.DELETE.toString()),
+                        new AntPathRequestMatcher(this.adminServer.getContextPath() + "/actuator/**"))
+                .and()
+                .rememberMe()
+                .key(UUID.randomUUID().toString())
+                .tokenValiditySeconds(1209600);
+
+
+
+        return http.build();
+    }
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
+    }
+
+
+}
